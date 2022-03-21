6e5e3d236a9f69047f82bb60a16c7923e7fc8997
grentank <60878292+grentank@users.noreply.github.com>
2022-03-21 23:59:46 +0300

    Adding  files

---
+++ app.js
@@ -0,0 +1,14 @@
+/*
+document.getElementById('randomWord').addEventListener('click', () => {
+    let randomIndex = Math.floor(Math.random()*(wordsDataLength-1));
+    document.getElementById('textbox').innerHTML = wordsData[randomIndex];
+})
+
+document.getElementById('findWords').addEventListener('click', () => {
+    for (let word of wordsData) {
+        if (!word.includes("я") && !word.includes("р") && !word.includes("п") && !word.includes("т") && !word.includes("с") && !word.includes("и") && !word.includes("а") && word[1] === "о" && !word.includes("н") && !word.includes("в") && !word.includes("м") && !word.includes("к") && !word.includes("й") && !word.includes("г") && !word.includes("з") && !word.includes("ы") && !word.includes("л"))
+            document.getElementById('textbox').innerHTML += word + "\n";
+    }
+})
+*/
+
